f5183097a979bedbaea3341364a006ce714c4d1b
Matheus Henrique <matheus.henrique@ceresinvestimentos.com>
2021-07-26 22:29:55 -0300

    add market project

---
+++ Market-master/src/pages/Cart/index.tsx
@@ -0,0 +1,127 @@
+import React from "react";
+import {
+  MdDelete,
+  MdAddCircleOutline,
+  MdRemoveCircleOutline,
+} from "react-icons/md";
+
+import { useCart } from "../../hooks/useCart";
+import { formatPrice } from "../../util/format";
+import { Container, ProductTable, Total } from "./styles";
+
+interface Product {
+  id: number;
+  title: string;
+  price: number;
+  image: string;
+  amount: number;
+}
+
+const Cart = (): JSX.Element => {
+  const { cart, removeProduct, updateProductAmount } = useCart();
+
+  // const cartFormatted = cart.map(product => ({
+  //   // TODO
+  // }))
+  // const total =
+  //   formatPrice(
+  //     cart.reduce((sumTotal, product) => {
+  //       // TODO
+  //     }, 0)
+  //   )
+
+  function handleProductIncrement(product: Product) {
+    // TODO
+  }
+
+  function handleProductDecrement(product: Product) {
+    // TODO
+  }
+
+  function handleRemoveProduct(productId: number) {
+    // TODO
+  }
+
+  return (
+    <Container>
+      <ProductTable>
+        <thead>
+          <tr>
+            <th aria-label="product image" />
+            <th>PRODUTO</th>
+            <th>QTD</th>
+            <th>SUBTOTAL</th>
+            <th aria-label="delete icon" />
+          </tr>
+        </thead>
+        <tbody>
+          {cart ? (
+            cart.map((product) => {
+              return (
+                <tr data-testid="product">
+                  <td>
+                    <img src={product.image} alt={product.title} />
+                  </td>
+                  <td>
+                    <strong>{product.title}</strong>
+                    <span>{formatPrice(product.price)}</span>
+                  </td>
+                  <td>
+                    <div>
+                      <button
+                        type="button"
+                        data-testid="decrement-product"
+                        // disabled={product.amount <= 1}
+                        // onClick={() => handleProductDecrement()}
+                      >
+                        <MdRemoveCircleOutline size={20} />
+                      </button>
+                      <input
+                        type="text"
+                        data-testid="product-amount"
+                        readOnly
+                        value={product.amount}
+                      />
+                      <button
+                        type="button"
+                        data-testid="increment-product"
+                        // onClick={() => handleProductIncrement()}
+                      >
+                        <MdAddCircleOutline size={20} />
+                      </button>
+                    </div>
+                  </td>
+                  <td>
+                    <strong>{product.amount * product.price}</strong>
+                  </td>
+                  <td>
+                    <button
+                      type="button"
+                      data-testid="remove-product"
+                      // onClick={() => handleRemoveProduct(product.id)}
+                    >
+                      <MdDelete size={20} />
+                    </button>
+                  </td>
+                </tr>
+              );
+            })
+          ) : (
+            <span>lista vazia</span>
+          )}
+        </tbody>
+      </ProductTable>
+
+      <footer>
+        <button type="button">Finalizar pedido</button>
+
+        <Total>
+          <span>TOTAL</span>
+          <strong>R$ 359,80</strong>
+        </Total>
+      </footer>
+    </Container>
+  );
+};
+
+export default Cart;
